55c8ed584e00accea3ff97c788e35e909bbd6e2c
Jeff Mu <jeffmc196@gmail.com>
2017-08-28 10:31:22 +0900

    Specify the input option for register page

--- ui/src/Register.js
+++ ui/src/Register.js
@@ -9,37 +9,43 @@ class PersonalInfo extends Component {
                 <h1>Personal Information</h1>
                 <div id="info_row">
                     <div id="info_option"><p>Full Name:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input"><input name="fullname" /></div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>User Name:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input"><input  name="username" /></div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>Date of Birth:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input"><input name="dateofbirth" /></div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>Email:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input"><input  name="email" /></div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>Gender:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input">
+                        <select name="gender">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="diverse">Gender diverse</option>
+                        </select>
+                    </div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>Password:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input"><input  name="password" /></div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>Confirm Password:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input"><input name="confirmpassword" /></div>
                 </div>
 
             </div>
@@ -56,12 +62,20 @@ class AdditionalInfo extends Component {
 
                 <div id="info_row">
                     <div id="info_option"><p>Current Occupation:</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input">
+                        <select name="occupation">
+                            <option value="student">Student</option>
+                        </select>
+                    </div>
                 </div>
 
                 <div id="info_row">
                     <div id="info_option"><p>What industry are you interested in?</p></div>
-                    <div id="info_input"><input /></div>
+                    <div id="info_input">
+                        <select name="interest">
+                            <option value="accounting">Accounting</option>
+                        </select>
+                    </div>
                 </div>
 
                 <div id="info_row">
@@ -85,7 +99,7 @@ class Submit extends Component {
             <div id="submit">
                 <div id="info_row">
                     <div id="info_option">
-                        <input id="checkBox" type="checkbox" />I accept the Terms and Conditions
+                        <input id="checkBox" type="checkbox" />I accept the <a>Terms and Conditions</a>
                     </div>
                     <div id="info_input">
                         <button>Submit</button>
@@ -102,9 +116,12 @@ class RegisterPanel extends Component {
     render() {
         return (
             <div id="register_panel">
-                <PersonalInfo />
-                <AdditionalInfo />
-                <Submit/>
+                <h2>Register</h2>
+                <form>
+                    <PersonalInfo />
+                    <AdditionalInfo />
+                    <Submit/>
+                </form>
             </div>
 
         );
