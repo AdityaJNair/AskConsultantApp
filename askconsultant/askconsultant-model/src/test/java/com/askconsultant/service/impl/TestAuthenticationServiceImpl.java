45163d71183e9dad2e17f3d4162da98f9f74872c
maxyaseen <30880836+maxyaseen@users.noreply.github.com>
2017-08-31 18:29:06 +1200

    Fixed the test case after password encryption integration with login service

--- askconsultant/askconsultant-model/src/test/java/com/askconsultant/service/impl/TestAuthenticationServiceImpl.java
+++ askconsultant/askconsultant-model/src/test/java/com/askconsultant/service/impl/TestAuthenticationServiceImpl.java
@@ -12,77 +12,79 @@ import com.askconsultant.service.dto.User;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.security.NoSuchAlgorithmException;
+
 public class TestAuthenticationServiceImpl {
 
 	private AuthenticationService authenticationService;
 	private UserDAO userDAO;
 	private EmployeeDAO employeeDAO;
-	
-	
+
 	@Before
-	public void init(){
+	public void init() {
 		authenticationService = new AuthenticationServiceImpl();
 		userDAO = mock(UserDAO.class);
 		employeeDAO = mock(EmployeeDAO.class);
-		((AuthenticationServiceImpl)authenticationService).userDAO = userDAO;
-		((AuthenticationServiceImpl)authenticationService).employeeDAO = employeeDAO;
+		((AuthenticationServiceImpl) authenticationService).userDAO = userDAO;
+		((AuthenticationServiceImpl) authenticationService).employeeDAO = employeeDAO;
 	}
-	
+
 	@Test
-	public void login_ValidUser(){
+	public void login_ValidUser() {
 		User user = getUserForValidUser();
 		user.setEmployee(false);
-		//set the mock expectations
-		when(userDAO.getUserByUserID(user.getUserID())).thenReturn(getDBUser());
-		when(employeeDAO.getEmployeeByUserID(user.getUserID())).thenReturn(getDBEmployee());
-		try{
+		try {
+			// set the mock expectations
+			when(userDAO.getUserByUserID(user.getUserID())).thenReturn(getDBUser());
+			when(employeeDAO.getEmployeeByUserID(user.getUserID())).thenReturn(getDBEmployee());
 			User login = authenticationService.login(user);
 			assertNotNull(login);
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
 	}
-	
+
 	@Test
-	public void login_InvalidUser(){
+	public void login_InvalidUser() {
 		User user = getUserForValidUser();
 		user.setEmployee(false);
-		//set the mock expectations
+		// set the mock expectations
 		when(userDAO.getUserByUserID(user.getUserID())).thenThrow((new InvalidUserException("User does not exist")));
-		try{
+		try {
 			authenticationService.login(user);
 			fail();
-		}catch(Exception e){
+		} catch (Exception e) {
 			assertEquals("User does not exist", e.getMessage());
 		}
 	}
-	
+
 	@Test
-	public void login_InvalidEmployee(){
+	public void login_InvalidEmployee() {
 		User user = getUserForValidUser();
 		user.setEmployee(true);
-		//set the mock expectations
-		when(employeeDAO.getEmployeeByUserID(user.getUserID())).thenThrow((new InvalidUserException("User does not exist")));
-		try{
+		// set the mock expectations
+		when(employeeDAO.getEmployeeByUserID(user.getUserID()))
+		.thenThrow((new InvalidUserException("User does not exist")));
+		try {
 			authenticationService.login(user);
 			fail();
-		}catch(Exception e){
+		} catch (Exception e) {
 			assertEquals("User does not exist", e.getMessage());
 		}
 	}
-	
+
 	@Test
-	public void login_ValidEmployee(){
+	public void login_ValidEmployee() {
 		User user = getUserForValidUser();
 		user.setEmployee(true);
-		//set the mock expectations
-		when(userDAO.getUserByUserID(user.getUserID())).thenReturn(getDBUser());
-		when(employeeDAO.getEmployeeByUserID(user.getUserID())).thenReturn(getDBEmployee());
-		try{
+		try {
+			// set the mock expectations
+			when(userDAO.getUserByUserID(user.getUserID())).thenReturn(getDBUser());
+			when(employeeDAO.getEmployeeByUserID(user.getUserID())).thenReturn(getDBEmployee());
 			User login = authenticationService.login(user);
 			assertNotNull(login);
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
@@ -92,18 +94,19 @@ public class TestAuthenticationServiceImpl {
 		User user = new User();
 		user.setEmployee(false);
 		user.setUserID("someID");
+		user.setPassword("password");
 		return user;
 	}
-	
-	private com.askconsultant.model.User getDBUser(){
+
+	private com.askconsultant.model.User getDBUser() throws NoSuchAlgorithmException {
 		com.askconsultant.model.User dbUser = new com.askconsultant.model.User();
-		dbUser.setPassword("encryptedPassword");
+		dbUser.setPassword(RegistrationServiceImpl.sha1("password"));
 		return dbUser;
 	}
-	
-	private com.askconsultant.model.Employee getDBEmployee(){
+
+	private com.askconsultant.model.Employee getDBEmployee() throws NoSuchAlgorithmException {
 		com.askconsultant.model.Employee dbEmployee = new com.askconsultant.model.Employee();
-		dbEmployee.setPassword("encryptedPassword");
+		dbEmployee.setPassword(RegistrationServiceImpl.sha1("password"));
 		return dbEmployee;
 	}
 }
