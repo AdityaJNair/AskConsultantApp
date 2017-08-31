f64204f3b64d2b38ae69f8504d8e370ffba6220a
DominicYuen <potatoe@fakeemail.com>
2017-08-31 12:19:12 +1200

    Fixed registrationJSONConverter

--- askconsultant/askconsultant-resource/src/main/java/com/askconsultant/resource/converter/RegistrationJSONConverter.java
+++ askconsultant/askconsultant-resource/src/main/java/com/askconsultant/resource/converter/RegistrationJSONConverter.java
@@ -1,5 +1,7 @@
 package com.askconsultant.resource.converter;
 
+import java.text.SimpleDateFormat;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import com.askconsultant.common.json.JsonReader;
@@ -11,23 +13,30 @@ import com.google.gson.JsonObject;
 @ApplicationScoped
 public class RegistrationJSONConverter {
 
-	public RegistrationDetails convert(String json) throws Exception {
+	public User convert(String json) throws Exception {
 		JsonObject jsonObject = JsonReader.readAsJsonObject(json);
-		final RegistrationDetails details = new RegistrationDetails();
-		details.(JsonReader.getStringOrNull(jsonObject, "userid"));
-		details.setPassword(JsonReader.getStringOrNull(jsonObject, "password"));
-		details.setEmployee((Boolean.parseBoolean(JsonReader.getStringOrNull(jsonObject, "isEmployee"))));
+		final User user = new User();
+		user.setFirstName(JsonReader.getStringOrNull(jsonObject, "firstname"));
+		user.setLastName(JsonReader.getStringOrNull(jsonObject, "lastname"));
+		user.setPreferredName(JsonReader.getStringOrNull(jsonObject, "prefferedname"));
+		user.setEmail(JsonReader.getStringOrNull(jsonObject, "email"));
+		user.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse(JsonReader.getStringOrNull(jsonObject, "dob")));
+		user.setOccupation(JsonReader.getStringOrNull(jsonObject, "prefferedname"));
+		user.setGender(JsonReader.getStringOrNull(jsonObject, "gender"));
+		user.setIndustry(JsonReader.getStringOrNull(jsonObject, "industry"));
+		user.setInterest(JsonReader.getStringOrNull(jsonObject, "interest"));
+		user.setSource(JsonReader.getStringOrNull(jsonObject, "source"));
 		return user;
 	}
 	
-	public JsonElement convertToJsonElement(final com.askconsultant.service.dto.User user){
-		final JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("userid", user.getUserID());
-		jsonObject.addProperty("firstName", user.getFirstName());
-		jsonObject.addProperty("lastName", user.getLastName());
-		jsonObject.addProperty("token", user.getToken());
-		return jsonObject;
-	}
+//	public JsonElement convertToJsonElement(final com.askconsultant.service.dto.User user){
+//		final JsonObject jsonObject = new JsonObject();
+//		jsonObject.addProperty("userid", user.getUserID());
+//		jsonObject.addProperty("firstName", user.getFirstName());
+//		jsonObject.addProperty("lastName", user.getLastName());
+//		jsonObject.addProperty("token", user.getToken());
+//		return jsonObject;
+//	}
 	
 //	public JsonElement convertToJsonElement(final List<Message> messages){
 //		JsonArray jsonArray = new JsonArray();
