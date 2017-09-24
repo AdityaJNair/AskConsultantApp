17f9cf77bab25824aaec60f46ba4b8c3d8d38b0e
yaseen <maxyaseen@gmail.com>
2017-09-25 12:33:13 +1300

    Employee registration rest api
    Tested on public server. Need to add unit tests.

--- askconsultant/askconsultant-resource/src/main/java/com/askconsultant/resource/EmployeeResource.java
+++ askconsultant/askconsultant-resource/src/main/java/com/askconsultant/resource/EmployeeResource.java
@@ -12,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.askconsultant.common.ResourceConstants;
+import com.askconsultant.common.json.JsonWriter;
+import com.askconsultant.exception.EmployeeExistsException;
 import com.askconsultant.resource.converter.OperationFailureJSONConvertor;
 import com.askconsultant.resource.converter.RegistrationJSONConverter;
 import com.askconsultant.service.RegistrationService;
@@ -20,7 +22,7 @@ import com.askconsultant.service.RegistrationService;
  * Contains methods for managing registration for employee
  *
  */
-@Path("/employee")
+@Path("/admin/employee")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class EmployeeResource {
@@ -47,10 +49,15 @@ public class EmployeeResource {
 	public Response register(final String json) throws Exception {
 		logger.debug("Registering employee");
 
-		com.askconsultant.service.dto.User userDetails = registrationJSONConverter.convertEmployeeRegistrationJSON(json);
-		registrationService.register(userDetails);
+		com.askconsultant.service.dto.User userDetails = registrationJSONConverter
+				.convertEmployeeRegistrationJSON(json);
 		try {
+			registrationService.registerEmployee(userDetails);
 			return Response.status(ResourceConstants.HTTP_RESPONSE_OK).build();
+		} catch (EmployeeExistsException e) {
+			return Response.status(ResourceConstants.HTTP_RESPONSE_UNAUTHORIZED_ERROR)
+					.entity(JsonWriter.writeToString(opFailureJSONConverter.convertToJsonElement(e.getMessage())))
+					.build();
 		} catch (Exception e) {
 			return Response.status(ResourceConstants.HTTP_RESPONSE_GENERIC_ERROR).build();
 		}
