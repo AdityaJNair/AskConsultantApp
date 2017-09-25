614a1a431c09cb1670ae1a03fc6c4a7782b8c58f
AdiJNair <adijnair@gmail.com>
2017-09-26 10:24:02 +1300

    removed other, and added other to subtopics

--- ui/src/containers/dumb/Admin/topics.js
+++ ui/src/containers/dumb/Admin/topics.js
@@ -1,7 +1,6 @@
 export const consultantsTopics = ["Development","Everyday Deloitte", "Human Capital", "Strategy & Operations", "Technology"];
-export const development = ["Oracle practice","SAP practice"];
-export const strategyAndOperations = ["Finance","Mergers & Acquisitions", "Operations","Strategy","Supply Chain"];
-export const everydayDeloitte = ["As a Junior","As an Intern","As a Manager","Other query"];
-export const humanCapital = ["HR transformation","Leadership","People analytics","Strategic change and organisation transformation","Talent"];
-export const technology = ["Analytics and information management","Systems integration","Technology architecture and strategy"];
-export const other = ["Other"];
+export const development = ["Oracle practice","SAP practice","Other"];
+export const strategyAndOperations = ["Finance","Mergers & Acquisitions", "Operations","Strategy","Supply Chain","Other"];
+export const everydayDeloitte = ["As a Junior","As an Intern","As a Manager","Other query","Other"];
+export const humanCapital = ["HR transformation","Leadership","People analytics","Strategic change and organisation transformation","Talent","Other"];
+export const technology = ["Analytics and information management","Systems integration","Technology architecture and strategy","Other"];
