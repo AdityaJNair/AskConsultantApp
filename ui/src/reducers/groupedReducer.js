d30eaeea4b2dbb1a29b32b3d8525424ab410a021
AdiJNair <adijnair@gmail.com>
2017-09-29 13:45:17 +1300

    Added merge issues

--- ui/src/reducers/groupedReducer.js
+++ ui/src/reducers/groupedReducer.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux'
 import registrationReducer from "./registrationReducer";
 import loginReducer from "./loginReducer";
-<<<<<<< HEAD
-import  messengerReducer from "./messengerReducer";
-=======
 import messengerReducer from "./messengerReducer";
->>>>>>> master
 
 const reducers = combineReducers({
     loginInfo: loginReducer,
