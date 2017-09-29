f9609d594112e73733abefda88a559ac53a71231
JunoJin <jjin525@aucklanduni.ac.nz>
2017-09-29 14:25:50 +1300

    removed all navbar buttons except settings and added menu ot it

--- ui/src/components/horizontalNavigation/HorizontalNavBar.js
+++ ui/src/components/horizontalNavigation/HorizontalNavBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './stylesheet/HorizontalNavBar.css';
 import FontIcon from 'react-md/lib/FontIcons';
+import MenuButton from 'react-md/lib/Menus/MenuButton';
+import ListItem from 'react-md/lib/Lists/ListItem';
 import logo from "../../images/deloitte_logo.png";
 
 class HorizontalNavBar extends Component {
@@ -11,14 +13,20 @@ class HorizontalNavBar extends Component {
                     <img src={logo} />
                 </div>
                 <div id="nav_container">
+                        {/*<div class="nav_item">*/}
+                            {/*<FontIcon id = "nav_icon_notification">notifications none</FontIcon>*/}
+                        {/*</div>*/}
+                        {/*<div class="nav_item">*/}
+                            {/*<FontIcon id = "nav_icon_sms">sms</FontIcon>*/}
+                        {/*</div>*/}
                     <div id="nav_item">
-                        <FontIcon id = "nav_icons">notifications none</FontIcon>
-                    </div>
-                    <div id="nav_item">
-                        <FontIcon id = "nav_icons">sms</FontIcon>
-                    </div>
-                    <div id="nav_item">
-                        <FontIcon id = "nav_icons">settings</FontIcon>
+                        <MenuButton
+                        id="nav_icons"
+                        icon
+                        menuItems={['Logout']}
+                        >
+                            settings
+                        </MenuButton>
                     </div>
                 </div>
             </div>
