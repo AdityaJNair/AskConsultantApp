2ef42273ea53ea4ccdd64d78226b206482498499
AdiJNair <adijnair@gmail.com>
2017-10-10 18:18:05 +1300

    removed hello world message

--- ui/src/components/messenger/MessengerBox/MessageWindow.js
+++ ui/src/components/messenger/MessengerBox/MessageWindow.js
@@ -14,14 +14,14 @@ class MessageWindow extends Component {
         console.log(`Render And Get the activeConvo: ${this.props.activeConvo}`)
         return (
             // If the activeConve is available, then the messenger view will display messages
-            // in the active conversation. 
+            // in the active conversation.
             this.props.activeConvo !== '' ?
                 <div id="user-message-window">
                     <MessageView/>
                     <MessengerSendText/>
                 </div> :
                 <div>
-                    Hello World!
+                
                 </div>
         )
     }
