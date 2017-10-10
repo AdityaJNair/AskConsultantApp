5dbeca8ca1721c0faa4464e8679bdf34ff1c0d07
Jeff Mu <jeffmc196@gmail.com>
2017-10-10 09:27:31 +0900

    Add question title for message window

---
+++ ui/src/components/messenger/MessengerBox/MessageQuestion.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MessageBubbleContainer from "./Message/MessageBubbleContainer";
+import './stylesheet/MessageWindow.css'
+
+class MessageQuestion extends Component {
+    render(){
+        return (
+            <div id="message-quetion">
+                <h2>Question here</h2>
+            </div>
+        )
+    }
+}
+
+export default MessageQuestion ;
--- ui/src/components/messenger/MessengerBox/MessageView.js
+++ ui/src/components/messenger/MessengerBox/MessageView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import MessageBubbleContainer from "./Message/MessageBubbleContainer";
+import MessageQuestion from "./MessageQuestion";
 
 class MessageView extends Component {
     render(){
         return (
             <div id="message-view">
+				<MessageQuestion />
                 <MessageBubbleContainer/>
             </div>
         )
